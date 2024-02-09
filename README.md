7a2a16b91698405df1a2b2c5a0a415ed570cc54f
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-09 16:09:19 -0500

    First version of the app

---
+++ README.md
@@ -0,0 +1,151 @@
+# HotKeyPad
+
+HotKeypad is a lightweight package that provides you with a keyboard shortcuts interface for your website.
+Built with vanilla JS, no external dependencies. Heavily inspired by [NinjaKeys](https://github.com/ssleptsov/ninja-keys).
+
+## Installation
+
+You can install hotkeypad via npm, pnpm or yarn. Here's how you can do it with npm:
+
+```bash
+npm install hotkeypad
+```
+
+## Usage
+
+To use HotKeyPad, you need to import the package and create a new instance of the HotKeyPad class. You can then add commands to the keypad and display it on your web app.
+
+First, add thee following tag to your page:
+```html
+<div id="hotkeypad"></div>
+```
+
+Then, you can use the following code to add the keypad to your page:
+```javascript
+import HotKeyPad from 'hotkeypad'
+
+const hotkeypad = new HotKeyPad()
+
+// This will set the commands and render the keypad automatically
+hotkeypad.setCommands([
+  {
+    section: "Actions",
+    id: "print",
+    title: "Print Page",
+    icon: `#custom-icon-print#`, // You can insert your own icon here, being and svg, image or font icon
+    hotkey: `${hotKeyPad.activationKey} + P`, // You can use the default activation key or set your own
+    handler: () => {
+      window.print()
+    }
+  },
+])
+
+// You can also add a listener to the open and close events
+window.addEventListener('hotkeypad:open', () => {
+  console.log('HotKeyPad is open')
+})
+
+window.addEventListener('hotkeypad:close', () => {
+  console.log('HotKeyPad is closed')
+})
+```
+The default activation key is `Ctrl` for Windows/Linux and `Cmd` for Mac.
+
+## Features
+
+- Keyboard navigation
+- Customizable commands
+- Light and dark mode support
+- Customizable colors with CSS variables
+- Customizable activation key
+
+## Attributes
+You can pass the following attributes to the `HotKeyPad` constructor or to the html tag (data- prefix is required with kebab-case for the attribute name).
+
+| Field       | Default        | Description                                             |
+|-------------|----------------|---------------------------------------------------------|
+| placeholder | Search command | Placeholder text for the search input.                  |
+| activationLetter | `K` | The activation letter for the commands. Default is `K`.       |
+| closeKey | `Escape` | The key to close the keypad. Default is `Escape`.                |
+| hideBreadcrumbs | false | Hide the breadcrumbs.                                        |
+
+## Commands Format
+Array of HotKeyPadData that represent the commands that will be displayed on the keypad.
+
+| Field     | Type     | Description                                                                 |
+|-----------|----------|-----------------------------------------------------------------------------|
+| id        | string   | The unique identifier of the command.                                       |
+| title     | string   | The title of the command.                                                   |
+| hotkey    | string   | The hotkey combination for the command. Format: `{ActivationKey}+{Letter}`  |
+| handler   | Function | The function to be executed when the command is triggered.                  |
+| icon      | string   | The icon of the command.                                                    |
+| section   | string   | The section of the command. Allows you to group commands.                   |
+
+## Methods
+
+### `setCommands(commands: HotKeyPadData[]): void`
+Set the commands that will be displayed on the keypad.  
+
+### `open(): void`
+Open the keypad.
+
+### `close(): void`
+Close the keypad.
+
+### `toggle(): void`
+Toggle the keypad.
+
+## Events
+
+### `hotkeypad:open`
+Fired when the keypad is opened.
+
+### `hotkeypad:close`
+Fired when the keypad is closed.
+
+## CSS Variables
+
+You can customize the colors of the keypad by changing the CSS variables. Here are the available variables:
+
+```css
+--hotkeypad-bg-kbd: #f9fafb;
+--hotkeypad-bg-backdrop: #fff;
+--hotkeypad-bg-container: #fff;
+--hotkeypad-bg-item-hover: #f3f4f6;
+--hotkeypad-border-container: #d1d5db;
+--hotkeypad-border-container-hover: #9ca3af;
+--hotkeypad-fg-muted: #4b5563;
+```
+
+Example:
+```css
+#hotkeypad {
+  --hotkeypad-bg-container: red;
+}
+```
+
+## CSS HotKeyPad Parts
+Allowing you to style specific elements from your style.
+  
+```css
+#hotkeypad [data-backdrop] {}
+#hotkeypad [data-container] {}
+#hotkeypad [data-sections] {}
+#hotkeypad [data-section] {}
+#hotkeypad [data-hotkey] {}
+#hotkeypad [data-hotkey][data-active] {}
+```
+
+## Icons
+You can use any icon library or custom icons. You can use the `icon` field to set the icon of the command. You can use an image, svg or font icon:
+- `<svg ...><path d="..." /></svg>`
+- `<img src="path/to/image.png" alt="Title" />`
+- `<i class="material-icons">print</i>`
+
+By default will use the [Simple Icons](https://simpleicons.org/) library as this: `<img src="https://cdn.simpleicons.org/{icon}" alt="{title}" />` if you pass a normal name instead of the any previous mentioned tags.
+
+## License
+
+Copyright (c) [Jesus Borrero](https://me.jesubohrdev.com).
+
+Licensed under the [MIT License](LICENSE)
---
+++ src/index.js
@@ -0,0 +1,428 @@
+/**
+ * @typedef {Object} HotKeyPadData
+ * @property {string} id
+ * @property {string} title
+ * @property {string} icon
+ * @property {string} hotkey
+ * @property {string} section
+ * @property {() => void} handler
+ */
+
+export default class HotKeyPad {
+  /** @type {HotKeyPadData[]} */
+  #commands = []
+  instance
+  /** @type {HTMLElement} */
+  #backdrop
+  /** @type {HTMLElement} */
+  #container
+  currentIndex = 0
+  #activationKey
+  #activationLetter = 'K'
+  #closeKey = 'Escape'
+  #placeholder = 'Search command'
+  #hideBreadcrumbs = false
+  // eslint-disable-next-line no-undef
+  #observer = new MutationObserver(this.#observeClassChanges.bind(this))
+  #iconsColor = 'black'
+
+  /**
+   * @description Creates an instance of the HotKeyPad class
+   * @param {Object} options
+   * @param {string} options.placeholder Placeholder for the search input
+   * @param {boolean} options.hideBreadcrumbs Whether to hide the breadcrumbs or not
+   * @param {string} options.activationLetter The activation letter for the pad. Default is "K"
+   * @param {string} options.closeKey The key to close the pad. Default is "Escape"
+   * @returns {HotKeyPad}
+   */
+  constructor ({
+    placeholder,
+    hideBreadcrumbs,
+    activationLetter,
+    closeKey
+  } = {}) {
+    this.instance = document.getElementById('hotkeypad')
+    this.#activationKey = navigator.userAgent.includes('Macintosh')
+      ? 'Cmd'
+      : 'Ctrl'
+
+    if (placeholder && placeholder !== '') this.#placeholder = placeholder
+    if (hideBreadcrumbs) this.#hideBreadcrumbs = hideBreadcrumbs
+    if (activationLetter && activationLetter !== '') { this.#activationLetter = activationLetter }
+    if (closeKey && closeKey !== '') this.#closeKey = closeKey
+
+    this.#checkTagOptions()
+    this.#init()
+    return this
+  }
+
+  #checkTagOptions () {
+    if (
+      this.instance.hasAttribute('data-placeholder') &&
+      this.instance.getAttribute('data-placeholder') !== ''
+    ) {
+      this.#placeholder = this.instance.getAttribute('data-placeholder')
+    }
+
+    if (
+      this.instance.hasAttribute('data-activation-letter') &&
+      this.instance.getAttribute('data-activation-letter') !== ''
+    ) {
+      this.#activationLetter = this.instance
+        .getAttribute('data-activation-letter')
+        .toUpperCase()
+    }
+
+    if (
+      this.instance.hasAttribute('data-close-key') &&
+      this.instance.getAttribute('data-close-key') !== ''
+    ) {
+      this.#closeKey = this.instance
+        .getAttribute('data-activation-letter')
+        .toUpperCase()
+    }
+
+    if (this.instance.hasAttribute('data-hide-breadcrumbs')) {
+      this.#hideBreadcrumbs = true
+    }
+  }
+
+  #init () {
+    // Listen for the activation key
+    document.addEventListener('keydown', (event) => {
+      const keyCode = `Key${this.#activationLetter.toUpperCase()}`
+      if (event.code === keyCode && (event.metaKey || event.ctrlKey)) {
+        event.preventDefault()
+        this.#isOpen ? this.close() : this.open()
+      }
+      if (event.key === this.#closeKey) this.close()
+    })
+
+    // Observe the class changes on the hotkeypad instance
+    this.#observer.observe(this.instance, {
+      attributes: true,
+      attributeFilter: ['class'],
+      childList: false,
+      characterData: false
+    })
+
+    // Render first blocks of the hotkeypad
+    this.#createBackdrop()
+    this.#createContainer()
+    this.#createHeader()
+    this.#createFooter()
+  }
+
+  /**
+   * Observes the class changes on the hotkeypad instance
+   * @param {MutationRecord[]} event
+   * @returns {void}
+   */
+  #observeClassChanges (event) {
+    const { attributeName, target } = event[0]
+    if (attributeName === 'class') {
+      if (target.classList.contains('dark')) {
+        this.#iconsColor = 'white'
+      } else {
+        this.#iconsColor = 'black'
+      }
+      this.#render()
+    }
+  }
+
+  /**
+   * Sets the commands for the hotkeypad and renders it
+   * @param {HotKeyPadData[]} commands Data to be set
+   */
+  setCommands (commands) {
+    this.#commands = commands
+    this.#render()
+    this.#setListeners()
+  }
+
+  #setListeners () {
+    // Listen for hotkey combinations registered in the hotkeypad
+    this.instance.addEventListener('keydown', (event) => {
+      if (event.metaKey || event.ctrlKey) {
+        this.#commands.find(({ hotkey, handler }) => {
+          const pairKey = hotkey
+            .split('+')
+            .map((key) => key.trim())[1]
+            .toLowerCase()
+          if (event.key.toLowerCase() === pairKey) {
+            event.preventDefault()
+            if (handler != null) handler()
+            this.close()
+          }
+          return false
+        })
+      }
+    })
+
+    // Listen for the search input
+    this.#container.addEventListener('input', (event) => {
+      const value = event.target.value.toLowerCase()
+      const sections = this.#container.querySelectorAll('[data-section]')
+      sections.forEach((section) => {
+        const list = section.querySelector('ul')
+        const items = list.querySelectorAll('li')
+
+        items.forEach((item) => {
+          const title = item.querySelector('p').innerText.toLowerCase()
+          if (title.includes(value)) item.style.display = 'flex'
+          else item.style.display = 'none'
+        })
+      })
+    })
+
+    // Listen for click events on the items
+    this.#items.forEach((item) => {
+      item.addEventListener('click', () => {
+        const hotkey = item.getAttribute('data-hotkey')
+        const currentItem = this.#commands.find(
+          (info) => info.hotkey === hotkey
+        )
+        currentItem?.handler()
+        this.close()
+      })
+    })
+
+    // Listen for the keyboard navigation events
+    this.#container.addEventListener('keydown', (event) => {
+      const items = this.#items
+      this.currentIndex = Array.from(items).findIndex((item) =>
+        item.hasAttribute('data-active')
+      )
+      this.currentIndex = this.currentIndex === -1 ? 0 : this.currentIndex
+      let nextIndex = 0
+
+      if (event.key === 'Enter') {
+        event.preventDefault()
+        items[this.currentIndex].click()
+        items[this.currentIndex].removeAttribute('data-active')
+        this.currentIndex = 0
+      }
+
+      if (event.key === 'ArrowUp') {
+        event.preventDefault()
+        nextIndex =
+          this.currentIndex - 1 < 0 ? items.length - 1 : this.currentIndex - 1
+      }
+
+      if (event.key === 'ArrowDown') {
+        event.preventDefault()
+        nextIndex =
+          this.currentIndex + 1 > items.length - 1 ? 0 : this.currentIndex + 1
+      }
+
+      if (event.key === 'Tab') {
+        event.preventDefault()
+        nextIndex =
+          this.currentIndex + 1 > items.length - 1 ? 0 : this.currentIndex + 1
+      }
+
+      items[this.currentIndex].removeAttribute('data-active')
+      items[nextIndex].setAttribute('data-active', '')
+    })
+
+    // Listen for the mouse over event on the items
+    this.#container.addEventListener('mouseover', (event) => {
+      if (event.target.tagName === 'LI') {
+        this.#items.forEach((item) => item.removeAttribute('data-active'))
+        event.target.setAttribute('data-active', '')
+      }
+    })
+  }
+
+  get #isOpen () {
+    return this.instance.style.visibility === 'visible'
+  }
+
+  get activationKey () {
+    return this.#activationKey
+  }
+
+  /**
+   * Returns the URL of the icon
+   * @param {string} icon Name of the icon
+   * @returns {string} URL of the icon
+   */
+  #iconURL (icon) {
+    return `https://cdn.simpleicons.org/${icon}/${this.#iconsColor}`
+  }
+
+  /**
+   * @type {Array<[string, HotKeyPadData[]]>}
+   */
+  get #sections () {
+    const map = new Map()
+    this.#commands.forEach((item) => {
+      const key = item.section
+      const collection = map.get(key)
+      if (!collection) {
+        map.set(key, [item])
+      } else {
+        collection.push(item)
+      }
+    })
+    return Array.from(map)
+  }
+
+  get #items () {
+    return this.#container.querySelectorAll('li')
+  }
+
+  open () {
+    window.dispatchEvent(new CustomEvent('hotkeypad:open')) // Allow to listen for the open event
+    this.instance.style.visibility = 'visible'
+    this.instance.style.opacity = '1'
+    this.instance.style.pointerEvents = 'auto'
+    setTimeout(() => this.#container.querySelector('input').focus(), 200)
+  }
+
+  close () {
+    window.dispatchEvent(new CustomEvent('hotkeypad:close')) // Allow to listen for the close event
+    this.instance.style.visibility = 'hidden'
+    this.instance.style.opacity = '0'
+    this.instance.style.pointerEvents = 'none'
+    this.#container.querySelector('input').value = ''
+    this.#container.removeEventListener('keydown', () => {})
+    this.#container.removeEventListener('mouseover', () => {})
+    this.#container.removeEventListener('input', () => {})
+  }
+
+  #isCustomIcon (icon) {
+    return (
+      /<svg/.test(icon) || /<img/.test(icon) || /<i/.test(icon) || icon === ''
+    )
+  }
+
+  /**
+   * Creates an HTML element
+   * @param {string} tagName Tag name of the element to create
+   * @param {string} content Content of the element
+   * @returns Element
+   * @private
+   */
+  #createElement (tagName, content) {
+    const el = document.createElement(tagName)
+    el.innerText = content
+    return el
+  }
+
+  #createBackdrop () {
+    const backdropEl = this.#createElement('div', '')
+    backdropEl.setAttribute('data-backdrop', '')
+    backdropEl.setAttribute('aria-hidden', true)
+
+    this.#backdrop = backdropEl
+    this.#backdrop.addEventListener('click', () => this.close())
+    this.instance.appendChild(backdropEl)
+  }
+
+  #createContainer () {
+    const containerEl = this.#createElement('div', '')
+    containerEl.setAttribute('data-container', '')
+    this.#container = containerEl
+    this.instance.appendChild(containerEl)
+  }
+
+  #createHeader () {
+    const headerEl = this.#createElement('header', '')
+    const inputEl = this.#createElement('input', '')
+    inputEl.setAttribute('type', 'text')
+    inputEl.setAttribute('placeholder', this.#placeholder)
+    inputEl.setAttribute('aria-label', this.#placeholder)
+    inputEl.setAttribute('autocomplete', 'off')
+    inputEl.setAttribute('spellcheck', 'false')
+
+    if (!this.#hideBreadcrumbs) {
+      const sectionTitleEl = this.#createElement('span', 'Home')
+      headerEl.appendChild(sectionTitleEl)
+    }
+
+    headerEl.appendChild(inputEl)
+    this.#container.appendChild(headerEl)
+  }
+
+  #createSections () {
+    const sectionsEl = this.#createElement('div', '')
+    sectionsEl.setAttribute('data-sections', '')
+
+    this.#sections.forEach(([section, commands]) => {
+      const sectionEl = this.#createElement('div', '')
+      sectionEl.setAttribute('data-section', section.toLowerCase())
+
+      const titleEl = this.#createElement('h4', section)
+      const listEl = this.#createElement('ul', '')
+
+      commands.forEach(({ title, icon, hotkey }) => {
+        const keys = hotkey.split('+').map((key) => key.trim())
+        const stringIcon = this.#isCustomIcon(icon)
+          ? icon
+          : `<img src="${this.#iconURL(icon)}" alt="${title}" />`
+
+        const itemEl = this.#createElement('li', '')
+        itemEl.setAttribute('data-hotkey', hotkey)
+        itemEl.setAttribute('tabindex', '0')
+
+        const itemIcon = this.#createElement('span', '')
+        itemIcon.innerHTML = stringIcon
+
+        const itemTitle = this.#createElement('p', '')
+        itemTitle.append(title)
+
+        const itemKeys = this.#createElement('div', '')
+        keys.forEach((key) => {
+          const keyEl = this.#createElement('span', key)
+          itemKeys.appendChild(keyEl)
+        })
+
+        itemEl.appendChild(itemIcon)
+        itemEl.appendChild(itemTitle)
+        itemEl.appendChild(itemKeys)
+        listEl.appendChild(itemEl)
+      })
+
+      sectionEl.appendChild(titleEl)
+      sectionEl.appendChild(listEl)
+      sectionsEl.appendChild(sectionEl)
+    })
+
+    // Append the sections between the header and footer
+    this.#container.insertBefore(sectionsEl, this.#container.lastChild)
+  }
+
+  #createFooter () {
+    const footerEl = this.#createElement('footer', '')
+
+    const keyEnter = this.#createElement('kbd', '↩')
+    const keyUp = this.#createElement('kbd', '↑')
+    const keyDown = this.#createElement('kbd', '↓')
+    const keyCmdK = this.#createElement(
+      'kbd',
+      `${this.#activationKey} + ${this.#activationLetter}`
+    )
+    const keyEsc = this.#createElement('kbd', this.#closeKey)
+
+    const pEnter = this.#createElement('p', ' to select')
+    pEnter.prepend(keyEnter)
+
+    const pUpDown = this.#createElement('p', ' to navigate')
+    pUpDown.prepend(keyUp, keyDown)
+
+    const pCmdK = this.#createElement('p', ' to close')
+    pCmdK.prepend(keyCmdK, keyEsc)
+
+    footerEl.append(pEnter, pUpDown, pCmdK)
+    this.#container.appendChild(footerEl)
+  }
+
+  #render () {
+    // Remove the previous sections and insert after the header
+    const sectionsEl = this.#container.querySelector('[data-sections]')
+    if (sectionsEl) sectionsEl.remove()
+    this.#createSections()
+    this.#items[0].setAttribute('data-active', '')
+  }
+}
