379da66c3bb18a9a0f4099bb5f652138ba7a04ab
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-11 23:46:29 -0500

    formatting

--- src/utils.ts
+++ src/utils.ts
@@ -4,9 +4,13 @@
  * @param props The props of the element
  * @returns The created element
  */
-export function createElement(tag: string, props?: { [key: string]: string } | string) {
+export function createElement(
+  tag: string,
+  props?: { [key: string]: string } | string
+) {
   const element = document.createElement(tag)
-  if(props && typeof props === "string" && props !== "") element.textContent = props
+  if (props && typeof props === "string" && props !== "")
+    element.textContent = props
   if (props && typeof props === "object")
     Object.keys(props).forEach((key) => {
       element.setAttribute(key, props[key])
